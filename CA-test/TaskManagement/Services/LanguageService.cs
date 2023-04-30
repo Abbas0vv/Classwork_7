5c32501fe09e5e1807996a88b1223eb268026698
User <User@WINDOWS-2R2MKCU>
2023-05-01 00:58:04 +0400

    This is all I can.

--- CA-test/TaskManagement/Services/LanguageService.cs
+++ CA-test/TaskManagement/Services/LanguageService.cs
@@ -17,21 +17,8 @@ namespace TaskManagement.Services
         {
             switch (CurrentLanguage)
             {
-                case "Az":
-                    RegisterCommand registerCommand = new RegisterCommand();
-                    registerCommand.Handle();
-                    break;
-                case "/login":
-                    LoginCommand loginCommand = new LoginCommand();
-                    loginCommand.Handle();
-                    break;
-                case "/update-language-preference":
-                    LanguageUpdateCommand languageUpdateCommand = new LanguageUpdateCommand();
-                    languageUpdateCommand.Handle();
-                    break;
-                case "/exit":
-                    Console.WriteLine("Bye-bye");
-                    return;
+                case "/aze":
+                    AzeLanguage //I CAN NOT 
                 default:
                     Console.WriteLine("Invalid command, pls try again");
                     break;
